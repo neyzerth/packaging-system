49690f9c9ef20f9bb4165495ae1d22bbaa638092
Axel <yerlan_2004@outlook.com>
2024-10-18 08:33:01 -0700

    Campos en los  que deberia de llevar indices

--- BaseDatos/Indices.sql
+++ BaseDatos/Indices.sql
@@ -1,1 +1,18 @@
 show  index from user
+
+--En la tabla tag campo barcode
+
+--Tabla packaging en campo tag
+
+--Tabla package en campo tag
+
+--Tabla package en campo tracking_code
+
+--Tabla product campo name
+
+--Tabla traceability campo product   --DUDAAAAAA
+
+--Tabla incident  cammpo date
+
+--Tabla report campo report_date   --DUDAAAAA
+
