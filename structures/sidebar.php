d28523ccdb4a171ede7bf3045a72ef494a033b0a
POLOK8039 <anonymous.pamb@gmail.com>
2024-11-07 23:10:33 -0800

    ...

--- structures/sidebar.php
+++ structures/sidebar.php
@@ -2,21 +2,21 @@
     <ul class="navegation">
         <li>
             <a class="nav-link" href="/" tooltip="Home">
-            <svg width="40" height="24" class="bi bi-house-door" viewBox="0 0 16 16">
+            <svg width="40" height="24" class="bi" viewBox="0 0 16 16">
                 <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z"/>
             </svg>
             </a>
         </li>
         <li>
             <a class="nav-link" href="<?php echo URL."boxes/"; ?>" tooltip="Boxes">
-            <svg width="40" height="24" class="bi bi-boxes" viewBox="0 0 16 16">
+            <svg width="40" height="24" class="bi" viewBox="0 0 16 16">
                 <path d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434zM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567zM7.5 9.933l-2.75 1.571v3.134l2.75-1.571zm1 3.134 2.75 1.571v-3.134L8.5 9.933zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567zm2.242-2.433V3.504L8.5 5.076V8.21zM7.5 8.21V5.076L4.75 3.504v3.134zM5.258 2.643 8 4.21l2.742-1.567L8 1.076zM15 9.933l-2.75 1.571v3.134L15 13.067zM3.75 14.638v-3.134L1 9.933v3.134z"/>
             </svg>
             </a>
         </li>
         <li>
             <a class="nav-link" href="<?php echo URL."materials/"; ?>" tooltip="Materials">
-            <svg width="40" height="24" class="bi bi-boxes" viewBox="0 0 16 16">
+            <svg width="40" height="24" class="bi" viewBox="0 0 16 16">
                 <path d="M4 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2m0 1a2 2 0 1 0 0-4 2 2 0 0 0 0 4m8-1a1 1 0 1 1 0-2 1 1 0 0 1 0 2m0 1a2 2 0 1 0 0-4 2 2 0 0 0 0 4M.115 3.18A.5.5 0 0 1 .5 3h15a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 14 12H2a.5.5 0 0 1-.491-.408l-1.5-8a.5.5 0 0 1 .106-.411zm.987.82 1.313 7h11.17l1.313-7z"/>
                 <path fill-rule="evenodd" d="M6 1a2.498 2.498 0 0 1 4 0c.818 0 1.545.394 2 1 .67 0 1.552.57 2 1h-2c-.314 0-.611-.15-.8-.4-.274-.365-.71-.6-1.2-.6-.314 0-.611-.15-.8-.4a1.497 1.497 0 0 0-2.4 0c-.189.25-.486.4-.8.4-.507 0-.955.251-1.228.638q-.136.194-.308.362H3c.13-.147.401-.432.562-.545a1.6 1.6 0 0 0 .393-.393A2.5 2.5 0 0 1 6 1"/>
             </svg>
@@ -24,7 +24,7 @@
         </li>
         <li>
             <a class="nav-link" href="<?php echo URL."process/"; ?>" tooltip="Process">
-            <svg width="40" height="24" class="bi bi-boxes" viewBox="0 0 16 16">
+            <svg width="40" height="24" class="bi" viewBox="0 0 16 16">
                 <path d="m11.42 2 3.428 6-3.428 6H4.58L1.152 8 4.58 2zM4.58 1a1 1 0 0 0-.868.504l-3.428 6a1 1 0 0 0 0 .992l3.428 6A1 1 0 0 0 4.58 15h6.84a1 1 0 0 0 .868-.504l3.429-6a1 1 0 0 0 0-.992l-3.429-6A1 1 0 0 0 11.42 1z"/>
                 <path d="M6.848 5.933a2.5 2.5 0 1 0 2.5 4.33 2.5 2.5 0 0 0-2.5-4.33m-1.78 3.915a3.5 3.5 0 1 1 6.061-3.5 3.5 3.5 0 0 1-6.062 3.5z"/>
             </svg>
@@ -32,28 +32,28 @@
         </li>
         <li>
             <a class="nav-link" href="<?php echo URL."products/"; ?>" tooltip="Products">
-            <svg width="40" height="24" class="bi bi-boxes" viewBox="0 0 16 16">
+            <svg width="40" height="24" class="bi" viewBox="0 0 16 16">
                 <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z"/>
             </svg>
             </a>
         </li>
         <li>
             <a class="nav-link" href="<?php echo URL."protocols/"; ?>" tooltip="Protocols">
-            <svg width="40" height="24" class="bi bi-boxes" viewBox="0 0 16 16">
+            <svg width="40" height="24" class="bi" viewBox="0 0 16 16">
                 <path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139q.323-.119.684-.12h5.396z"/>
             </svg>
             </a>
         </li>
         <li>
             <a class="nav-link" href="<?php echo URL."users/"; ?>" tooltip="Staff" style="border-bottom: none;">
-            <svg width="40" height="24" class="bi bi-boxes" viewBox="0 0 16 16">
+            <svg width="40" height="24" class="bi" viewBox="0 0 16 16">
                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
             </svg>
             </a>
         </li>
     </ul>
-    <a href="#" class="link-body dropdown-toggle" id="profile-toggle">
-        <img class="rounded-circle" src="https://github.com/mdo.png" width="24" height="24">
+    <a href="#" class="link-body dropdown-toggle" id="profile-toggle" tooltip="Staff">
+        <img class="bi" src="https://github.com/mdo.png" width="24" height="24">
     </a>
     <ul class="dropdown-menu" id="dropdown-menu">
         <li>
