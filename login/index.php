3ac21462aa2fab8ad2c08119652687968e3e9a62
POLOK8039 <anonymous.pamb@gmail.com>
2024-11-08 19:57:12 -0800

    Cambios menores

--- login/index.php
+++ login/index.php
@@ -26,7 +26,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="<?php echo STYLE . "!important.css" ?>">
     <link rel="stylesheet" href="<?php echo STYLE . "!color-palette.css" ?>">
     <link rel="stylesheet" href="<?php echo STYLE . "body.css" ?>">
     <link rel="stylesheet" href="<?php echo STYLE . "login.css" ?>">
@@ -34,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <body>
     <div class="overlay">
-        <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" fill="currentColor" class="bi bi-eye-slash" viewBox="0 0 16 16">
+        <svg width="120" height="120" class="bi" viewBox="0 0 16 16">
             <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z"/>
             <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829"/>
             <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z"/>
@@ -48,16 +47,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         <path d="M5.5 7V4.5a2.5 2.5 0 1 1 5 0V7H11a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h.5zm1-2.5V7h3V4.5a1.5 1.5 0 0 0-3 0zm1.5 6a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5z"/>
                     </svg>
                 </a>
-            <h4 id="toggleText">Forgot your password?</h4>
+            <h4 id="toggleText">Forgot password?</h4>
             <p id="paragraph">Select the icon for more information</p>
         </div>
-        <form class="division" action="/login/" id="rightDiv" method="post">
-            <h2>Welcome back!</h2>
-            <p><?php echo $msg ?></p>
-            <input id="username" name="username" class="form-control" type="text"  autocomplete="off" required placeholder="User">
-            <input id="password" name="password" class="form-control" type="password" autocomplete="off" required placeholder="Password">
-            <button type="submit" class="btn-primary">Login</button>
-        </form>
+        <div class="division" id="rightDiv">
+            <form action="#" method="post">
+                <h2>Welcome back!</h2>
+                <p><?php echo $msg ?></p>
+                <input id="username" name="username" class="form-control" type="text" autocomplete="off" required placeholder="User">
+                <input id="password" name="password" class="form-control" type="password" autocomplete="off" required placeholder="Password">
+                <button type="submit" class="btn-primary">Login</button>
+            </form>
+        </div>
     </main>
     <script src="script.js"></script>
 </body>
--- login/script.js
+++ login/script.js
@@ -5,8 +5,11 @@ const toggleImage = document.getElementById('toggleImage');
 const toggleText = document.getElementById('toggleText');
 const paragraph = document.getElementById('paragraph');
 const images = [
-    '../structures/svg/padlock-unlocked.svg',
-    '../structures/svg/user.svg'
+    `
+    <path d="M5.5 7V4.5a2.5 2.5 0 1 1 5 0V7H11a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h.5zm1-2.5V7h3V4.5a1.5 1.5 0 0 0-3 0zm1.5 6a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5z"/>
+    ` , `
+    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+    `
 ];
 
 link.addEventListener('click', event => {
@@ -22,28 +25,29 @@ link.addEventListener('click', event => {
         const isLogin = leftDiv.classList.contains('swapped');
 
         rightDiv.innerHTML = isLogin
-            ? `
-            <form action="\GitHub\packaking-system/boxes/index.php" id="rightDiv" method="post">
-                <strong style="font-size: 20px;">Request your code!</strong>
+            ?`
+            <form action="" method="post">
+                <h2>Request your code!</h2>
                 <p>We will send instructions to your email.</p>
-                <input id="username" name="username" class="form-control" type="text" autocomplete="off" required placeholder="Enter your email">
-                <input id="password" name="password" class="form-control" type="password" autocomplete="off" required placeholder="Confirm your email">
+                <input id="email" name="email" class="form-control" type="email"  autocomplete="off" required placeholder="Enter your email">
+                <input id="confirm-email" name=" confirm-email" class="form-control" type="email" autocomplete="off" required placeholder="Confirm your email">
                 <button type="submit" class="btn-primary">Next step</button>
-            </form>`
-            : `
-            <form action="\GitHub\packaking-system/boxes/index.php" id="rightDiv" method="post">
-                <strong style="font-size: 20px;">Hello again!</strong>
+            </form>
+            ` : `
+            <form action="" method="post">
+                <h2>Welcome back!</h2>
                 <p><?php echo $msg ?></p>
-                <input id="username" name="username" class="form-control" type="text" autocomplete="off" required placeholder="User">
+                <input id="username" name="username" class="form-control" type="text"  autocomplete="off" required placeholder="User">
                 <input id="password" name="password" class="form-control" type="password" autocomplete="off" required placeholder="Password">
                 <button type="submit" class="btn-primary">Login</button>
-            </form>`;
+            </form>
+            `;
 
-        toggleImage.src = images[+isLogin];
+        toggleImage.innerHTML = images[+isLogin];
 
         toggleText.textContent = isLogin ? 'Already have an account?' : 'Forgot password?';
 
-        paragraph.textContent = isLogin ? 'Select the icon to login.' : 'Select the icon to request instructions.';
+        paragraph.textContent = isLogin ? 'Select the icon to login' : 'Select the icon for more information';
 
         leftDiv.style.opacity = '1';
         rightDiv.style.opacity = '1';
