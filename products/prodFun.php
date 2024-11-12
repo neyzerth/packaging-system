0a4393e639ef5bb3f4fe1f60c10f66daf2f15736
Leyva <yerlan_2004@outlook.com>
2024-11-12 09:38:55 -0800

    Funciones para los sp de producto y protocolo

--- products/prodFun.php
+++ products/prodFun.php
@@ -1,4 +1,31 @@
 <?php
+    require_once "../config.php";
+function addProduct($code, $name, $description, $height, $width, $length, $weight, $packaging_protocol) {
+    $db = connectdb(); 
+
+    $query = "CALL addProduct(".
+        "'$code',".
+        "'$name',".
+        "'$description',".
+        "$height,".
+        "$width,".
+        "$length,".
+        "$weight,".
+        "$packaging_protocol".
+    ");";
+
+    echo "<p>$query</p>"; 
+
+    try {
+        return mysqli_query($db, $query);
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+}
+
+
+
+
 function getProducts(){
     $db = connectdb();
     $query = "SELECT code, name, description,". 
--- protocols/protocolFun.php
+++ protocols/protocolFun.php
@@ -1,4 +1,23 @@
 <?php
+    require_once "../config.php";
+
+    function addPackagingProtocol($name, $file_name) {
+        $db = connectdb(); 
+    
+        $query = "CALL addPackagingProtocol(".
+            "'$name',".
+            "'$file_name'".
+        ");";
+    
+        echo "<p>$query</p>"; 
+    
+        try {
+            return mysqli_query($db, $query);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+    
 function getProtocols(){
     $db = connectdb();
     $query = "SELECT num, name, file_name".
