e7e3246475060e83d1f97fdaa8c3000f4c59c091
Neyzer <neyzertoledo@gmail.com>
2024-11-22 05:22:11 -0800

    adding more forms styles

--- products/editProduct.php
+++ products/editProduct.php
@@ -30,42 +30,81 @@
         }
     }
 ?>
-<main class="tables">
+<main class="forms">
     <div class="background">
-        <a href="disableProduct.php?code=<?php echo $product['code']; ?>" onclick="return confirm('¿Estás seguro de que deseas desactivar este producto?');">Disable</a>
-        <table class="table">
-            <form action="" method="POST" autocomplete="off">
-                <input type="hidden" name="code" value="<?php echo $product['code']; ?>">
-                <label>Name:</label>
-                <input type="text" name="name" value="<?php echo $product['name']; ?>" required>
+        <form class="form" action="" method="post" autocomplete="off">
+            <header class="header">
+                <img src="<?php  echo SVG . "icon.svg" ?>">
+                <h1>Products</h1>
+            </header>
+            <h2>Products</h2>
+            <div class="rows">
+                <div class="row-sm-3">
+                    <a class="btn" href="disableProduct.php?code=<?php echo $product['code']; ?>" onclick="return confirm('¿Estás seguro de que deseas desactivar este producto?');">Disable</a>
+                </div>
+            </div>
+            <div class="rows">
+                <div class="row-sm-3">
+                    <h4 for="code">Code</h4>
+                    <div class="inputs">
+                        <input name="code" id="code" type="text" value="<?php echo $product['code']; ?>" required maxlength="5">
+                    </div>
+                </div>
+                <div class="row-sm-3">
+                    <h4 for="name">Product Name</h4>
+                    <div class="inputs">
+                        <input name="name" id="name" type="text" value="<?php echo $product['name']; ?>" required maxlength="50">
+                    </div>
+                </div>
+                <div class="row-sm-3">
+                    <h4 for="description">Description</h4>
+                    <div class="inputs">
+                        <input name="description" id="description" type="text" placeholder="fragile" value="<?php echo $product['description']; ?>" required maxlength="255">
+                    </div>
+                </div>
+                <div class="row-sm-3">
+                    <h4 for="height">Height</h4>
+                    <div class="inputs">
+                        <input name="height" id="height" type="number" value="<?php echo $product['height']; ?>" required >
+                    </div>
+                </div>
+                <div class="row-sm-3">
+                    <h4 for="width">Width</h4>
+                    <div class="inputs">
+                        <input name="width" id="width" type="number" value="<?php echo $product['width']; ?>" required >
+                    </div>
+                </div>
+                <div class="row-sm-3">
+                    <h4 for="length">Length</h4>
+                    <div class="inputs">
+                        <input name="length" id="length" type="number" value="<?php echo $product['length']; ?>" required >
+                    </div>
+                </div>
                 
-                <label>Description:</label>
-                <textarea name="description" required><?php echo $product['description']; ?></textarea>
-                
-                <label>Height:</label>
-                <input type="number" name="height" value="<?php echo $product['height']; ?>" required>
-
-                <label>Width:</label>
-                <input type="number" name="width" value="<?php echo $product['width']; ?>" required>
-
-                <label>Length:</label>
-                <input type="number" name="length" value="<?php echo $product['length']; ?>" required>
-
-                <label>Weight:</label>
-                <input type="number" name="weight" value="<?php echo $product['weight']; ?>" required>
-                
-                <label for="supervisor">Protocol</label>
-                <select name="packaging_protocol">
-                    <?php 
-                        while ($protocol = mysqli_fetch_assoc($protocols)):  
-                            $selected = $product['packaging_protocol'] === $protocol['num'] ? 'selected' : '';
-                            echo "<option value='{$protocol['num']}' $selected>{$protocol['file_name']}</option>";
-                        endwhile; 
-                    ?>
-                </select>
-                
-                <button type="submit">Update</button>
-            </form>
-        </table>
+                <div class="row-md-5">
+                    <h4 for="weight">Weight</h4>
+                    <div class="inputs">
+                        <input name="weight" id="weight" type="number" placeholder="999" value="<?php echo $product['weight']; ?>" required maxlength="10">
+                    </div>
+                </div>
+                <div class="row-md-5">
+                    <h4 for="packaging_protocol">Packaging Protocol</h4>
+                    <div class="inputs">
+                        <select class="input" required name="packaging_protocol" id="packaging_protocol options">
+                            <?php 
+                            while ($protocol = mysqli_fetch_assoc($protocols)):  
+                                $selected = $product['packaging_protocol'] === $protocol['num'] ? 'selected' : '';
+                                echo "<option value='{$protocol['num']}' $selected>{$protocol['file_name']}</option>";
+                            endwhile; 
+                            ?>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <footer class="footer">
+                <button class="btn-primary" type="submit">Confirm</button>
+            </footer>
+        </form>
     </div>
 </main>
--- protocols/addProtocol.php
+++ protocols/addProtocol.php
@@ -1,9 +1,5 @@
 <?php
     require "protocolFun.php";
-    
-    var_dump($_FILES['pdf']);
-    echo "<br>";
-    var_dump($_FILES);
 
     if($_SERVER['REQUEST_METHOD']=='POST' && isset($_FILES['pdf'])){
         $file = $_FILES['pdf'];
@@ -20,34 +16,31 @@
 
 ?>
 
-    <div class="container">
-        <main>
-            <div class="py5" style="text-align: center;">
-                <svg width="60" height="60" viewBox="0 0 16 16" fill="#5C73F2">
-                    <path
-                        d="M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.01-.003.268-.108a.75.75 0 0 1 .558 0l.269.108.01.003zM10.404 2 4.25 4.461 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339L8 5.961 5.596 5l6.154-2.461z" />
-                </svg>
-                <h1>Protocol form</h1>
-            </div>
-            <div class="row-div">
-                <div class="">
-                    <form action="" method="post" autocomplete="off" enctype="multipart/form-data">
-                        <div class="row-form">
-                            <div class="row-md-6">
-                                <label for="height">Name of protocol</label>
-                                <div class="input-group">
-                                    <input name="name" id="name" type="text" step="0.01" maxlength="50">
-                                </div>
-                            </div>
-                            <div class="row-md-6">
-                                <label for="width">Filename</label>
-                                <div class="input-group">
-                                    <input type="file" name="pdf" id="pdf" type="text" step="0.01" required accept=".pdf">
-                                </div>
-                            </div>
-                        <hr class="border-bottom" style="margin-top: 2rem; margin-bottom: 2rem;">
-                        <button class="btn-primary" type="submit">Confirm creation</button>
-                    </form>
+<main class="forms">
+    <div class="background">
+        <form class="form" action="" method="post" autocomplete="off" enctype="multipart/form-data">
+            <header class="header">
+                <img src="<?php  echo SVG . "icon.svg" ?>">
+                <h1>Materials</h1>
+            </header>
+            <h2>Material</h2>
+            <div class="rows">
+                <div class="row-md-5">
+                    <h4 for="name">Name of protocol</h4>
+                    <div class="inputs">
+                        <input name="name" id="name" type="text">
+                    </div>
                 </div>
+                <div class="row-md-5">
+                    <h4 for="pdf">File *</h4>
+                        <input class="inputs" name="pdf" id="pdf" type="file" accept=".pdf" required >
+                </div>
+
             </div>
-        </main>
+            <hr>
+            <footer class="footer">
+                <button class="btn-primary" type="submit">Confirm</button>
+            </footer>
+        </form>
+    </div>
+</main>
