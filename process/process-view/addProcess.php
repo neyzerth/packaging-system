dcc2558e91347701b9fe82d482209186dfcfe39b
POLOK8039 <anonymous.pamb@gmail.com>
2024-11-22 17:35:21 -0800

    pantalla processView

--- process/process-view/addProcess.php
+++ process/process-view/addProcess.php
@@ -1,23 +1,62 @@
-    <main>
-        <div>
-            <h1>PROCESS VIEW</h1>
-            <span>ID Traceability: XXXX</span>
-        </div>
-        <div>
-            <div>
+    <div class="tables">
+        <div class="background">
+            <?php $link="?a=add"; include HEADER; ?>
+            <div style="text-align: center">
+                <h1>PROCESS VIEW</h1>
+            </div>
+            <div class="process">
                 <div>
-                    <h4>Packing</h4>
+                    <h3>Packing</h3>
+                    <img class="bin" src="<?php echo SVG . "products.svg" ?>">
+                    <select class="inputs">
+                    </select>
                 </div>
                 <div>
-                    <h4>Warehouse</h4>
+                    <h3>Packaging</h3>
+                    <img class="bin" src="<?php echo SVG . "boxes.svg" ?>">
+                    <select class="inputs">
+                    </select>
                 </div>
                 <div>
-                    <h4>Packaging</h4>
+                    <h3>Warehouse</h3>
+                    <img class="bin" src="<?php echo SVG . "zone.svg" ?>">
+                    <select class="inputs">
+                    </select>
                 </div>
             </div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>
+                            <span>Code</span>
+                            <span class="column-order"></span>
+                        </th>
+                        <th>
+                            <span>Name</span>
+                            <span class="column-order"></span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th>
+                            <span>Code</span>
+                        </th>
+                        <th>
+                            <span>Name</span>
+                        </th>
+                    </tr>
+                </tfoot>
+            </table>
+            <?php include FOOTER ?>
+            <footer class="footer">
+                <button class="btn-primary" type="submit">Incident</button>
+            </footer>
         </div>
-        <div>
-            <div></div>
-            <div></div>
-        </div>
-    </main>
+    </div>
