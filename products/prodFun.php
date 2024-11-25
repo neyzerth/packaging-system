9a274bc16f08a7ed4365798420dae18880bea4a5
Neyzer <neyzertoledo@gmail.com>
2024-11-25 00:23:06 -0800

    borrador proccess view

--- products/prodFun.php
+++ products/prodFun.php
@@ -1,5 +1,5 @@
 <?php
-require_once "../config.php";
+require_once __DIR__ . "/../config.php";
 function addProduct($code, $name, $description, $height, $width, $length, $weight, $packaging_protocol) {
     $db = connectdb(); 
 
