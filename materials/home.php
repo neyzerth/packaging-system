c37bea8177a2a0f833d4798317872acab003940c
Arturo <arturoherreraluevano@gmail.com>
2024-12-03 02:17:28 -0800

    iconos agregados

--- materials/home.php
+++ materials/home.php
@@ -2,13 +2,13 @@
         <ul class="navegation">
             <li>
                 <a class="nav-link" href="<?php echo URL . "/materials/material/" ?>">
-                    <img class="bi" src="<?php echo SVG . "file.svg" ?>">
+                    <img class="bi" src="<?php echo SVG . "material.svg" ?>">
                     <h2>Material</h2>
                 </a>
             </li>
             <li>
                 <a class="nav-link" href="<?php echo URL . "/materials/unitOfMeasure/" ?>">
-                    <img class="bi" src="<?php echo SVG . "gear.svg" ?>">
+                    <img class="bi" src="<?php echo SVG . "unit-measure.svg" ?>">
                     <h2>Unit of measure</h2>
                 </a>
             </li>
